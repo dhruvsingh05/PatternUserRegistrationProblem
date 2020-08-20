9aa6bc396e8c1fe2acca77f91b5d423e6dce36ef
CAPTAIN <singhprakashgolu7351@gmail.com>
2020-08-21 00:07:46 +0530

    uc2

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,8 +3,15 @@ read -p "Enter your first name: " fname
 pattern="^[A-Z][a-z]{2,}$"
 if [[ $fname =~ $pattern ]]
 then
-echo "Done"
+	read -p "Enter your Last Name: " lname
+	pattern1="^[A-Z][a-z]{2,}$"
+		if [[ $lname =~ $pattern1 ]]
+		then
+			echo "Thank you"
+		else
+			echo "please enter your correct Last Name"
+		fi
 else
-echo "please enter your correct name"
+	echo "please enter your correct First Name"
 fi
 
