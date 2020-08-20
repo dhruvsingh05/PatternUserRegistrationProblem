be24b1484ad5cd5222c2dc3c1c482ef65c4aae76
CAPTAIN <singhprakashgolu7351@gmail.com>
2020-08-21 01:20:27 +0530

    uc4 mobile Number

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 read -p "Enter your first name: " fname
 pattern="^[A-Z][a-z]{2,}$"
 if [[ $fname =~ $pattern ]]
@@ -12,7 +12,17 @@ then
         		pattern2="^[0-9a-zA-Z]+([._+-][0-9a-zA-z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
                 	if [[ $email =~ $pattern2 ]]
                 	then
-				echo "Thank You"
+				
+				read -p "Enter your Mobile number (eg:91 1234567890 ): " m_no
+                        	pattern3="^[1-9]{2,2}([ ][1-9])([0-9]{9,9})$"
+                        	if [[ $m_no =~ $pattern3 ]]
+                        	then
+                                	echo "Thank You"
+                        	else
+                                	echo "please enter your correct mobile number"
+                        	fi
+
+
                 	else
                         	echo "please enter your correct Email Address"
                 	fi
